044efc99ee4fb401e56b7013cee1436c6ce2aa44
Jennings Chen <student3841701@gmail.com>
2020-02-11 14:19:03 +0800

    Add files via upload

---
+++ desco.R
@@ -0,0 +1,16 @@
+desco <- function(name){
+  switch(name,
+         viola=c("#752092","#c957bc","#ffc872","#ffe3b3"),
+         sea=c("#26648e","#4f8fc0","#53d2dc","#ffe3b3"),
+         cove=c("#006bbb","#30a0e0","#ffc872","#ffe3b3"),
+         strawberry=c("#5193b3","#62c4c3","#f8d49b","#f8e6cb"),
+         rose=c("#cc184e","#e84575","#f76cae","#ffe3b3"),
+         kiwi=c("#028174","#0ab68b","#92de8b","#ffe3b3"),
+         sunrise=c("#75bde0","#f8d49b","#f8bc9b","#f89b9b"),
+         stormii=c("#3b7097","#4a8db7","#75bde0","#a2e2f8"),
+         petals=c("#588c7e","#689581","#acbc8a","#ddd7ac"),
+         melon=c("#97dbae","#c3e5ae","#f1e1a6","#f4bbbb"),
+         kermit=c("#75bde0","#78d1d2","#97dbae","#cde4ad"),
+         beachside=c("#3b7097","#75bde0","#a9d09e","#f6e2bc")
+  )
+}
